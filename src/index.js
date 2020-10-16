862302dbadb00547ffe430d80d157fc57f48efd1
haraphat01 <haraphat@gmail.com>
2020-10-16 12:45:27 +0100

    Started with the logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 
 import './scss/style.scss';
+
+let taskForm = document.forms[0];
+let todoTitle = documentGetElementById('todoTitle')
+let todoDescription = documentGetElementById('todoDescription')
+let todoDueDate = documentGetElementById('todoDueDate')
+let todoPriority = documentGetElementById('todoPriority')
+let todoButton = documentGetElementById('todoButton')
+
